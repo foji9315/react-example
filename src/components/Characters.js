11400be85947e9b0f397071badce9ccc954f3536
Ivan F <57454456+foji9315@users.noreply.github.com>
2022-07-01 14:33:26 -0500

    finish

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -5,19 +5,22 @@ function Characters(params) {
         setCharacters(null);
     }
 
+    console.log(characters);
     return (
         <div className="characters">
             <h1 className="title">Characters</h1>
             <span id="backToHome1Button" className="back-home" onClick={backToHome}>Back to home</span>
+            <br></br>
+            <p>Showing from <span id="initValue" className="highlighted">{characters[0].id}</span> to <span id="endValue" className="highlighted">{characters[characters.length-1].id}</span></p>
             <div className="container-characters">
                 {characters.map((element, index) => (
-                    <div className="character-container" key={index}>
+                    <div id={element.id} className="character-container" key={index}>
                         <div>
                             <img src={element.image} alt={element.name} />
                         </div>
                         <div>
                             <h3>{element.name}</h3>
-                            <h6>
+                            <h6 id={`status${element.id}`}>
                                 {element.status === "Alive" ? (
                                     <>
                                         <span className="alive" />
@@ -32,11 +35,19 @@ function Characters(params) {
                             </h6>
                             <p>
                                 <span className="text-grey">Episodies: </span>
-                                <span>{element.episode.length}</span>
+                                <span id={`numEpisodies${element.id}`}>{element.episode.length}</span>
                             </p>
                             <p>
                                 <span className="text-grey">Specie: </span>
-                                <span>{element.species}</span>
+                                <span id={`specie${element.id}`}>{element.species}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Gender: </span>
+                                <span id={`gender${element.id}`}>{element.gender}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Ref: </span>
+                                <a target="_blank" id={`url${element.id}`} href={element.url} rel="noreferrer">Api reference</a>
                             </p>
                         </div>
                     </div>

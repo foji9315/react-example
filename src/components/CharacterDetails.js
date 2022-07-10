1eb32c0dc318549fdc17c8630ebac33a4c8ab4bc
Ivan F <57454456+foji9315@users.noreply.github.com>
2022-07-10 18:35:12 -0500

    Adding id's

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -12,16 +12,16 @@ function CharacterDetails(params) {
             <span id="backToPage1Button" className="back-home" onClick={backToPage}>Back to List</span>
             <hr></hr>
             <h1>{character.name}</h1>
-            <div className="card border-primary" >
+            <div className="card border-primary text-bg-dark" >
                 <img src={character.image} className="card-img-top border-primary" alt="..." />
                 <div className="card-body">
-                    <h2 className="card-title text-info fs-2 text">{character.name}</h2>
+                    <h2 className="card-title text-info fs-2 text"  id="name">{character.name}</h2>
                     <p className="card-text">It is a character originally from
-                        <span className="highlighted"> {character.origin.name}</span> and created at :
-                        <span className="highlighted"> {character.created}</span> Located at :
-                        <span className="highlighted"> {character.location.name}</span> whose gender is
-                        <span className="highlighted"> {character.gender}</span>. He participates on : 
-                        <span className="highlighted"> {character.episode.length}</span>
+                        <span id="origin" className="highlighted"> {character.origin.name}</span> and created at :
+                        <span id="createdAt" className="highlighted"> {new Date(character.created).toLocaleString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</span> Located at :
+                        <span id="location" className="highlighted"> {character.location.name}</span> whose gender is
+                        <span id="gender" className="highlighted"> {character.gender}</span>. He participates on : 
+                        <span id="numOfEpisodies" className="highlighted"> {character.episode.length}</span> episodies
                         <hr />
                     </p>
                 </div>
@@ -32,10 +32,10 @@ function CharacterDetails(params) {
                 </p>
                 <div>
                     <div className="collapse collapse-horizontal" id="collapseWidthExample">
-                        <div className="card card-body">
+                        <div className="card card-body border-primary text-bg-dark">
                             <ul className="list-group list-group-flush">
                                 {character.episode.map((element, index) => (
-                                    <li className="list-group-item">{element}</li>
+                                    <li key={index} className="list-group-item text-white bg-dark border-success">{element}</li>
                                 ))}
                             </ul>
                         </div>
